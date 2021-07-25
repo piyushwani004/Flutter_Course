7872f900cd5ab6abb5e6ae71f269af6b22f1f946
Piyush wani <piyushwani04@gmail.com>
2021-07-25 11:42:13 +0530

    add netwok image and drawer hader

--- lib/Pages/my_drawer.dart
+++ lib/Pages/my_drawer.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
 
-  const MyDrawer({ Key? key }) : super(key: key);  
+  final imageUrl = "https://xenforo.com/community/data/avatars/o/202/202502.jpg?1587654225";
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      
+      child: ListView(
+        children: [
+          DrawerHeader(
+            padding: EdgeInsets.zero,
+            child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text("Piyush Wani"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+                accountEmail: Text("piyushwani04@gmail.com")),
+          ),
+        ],
+      ),
     );
   }
 }
